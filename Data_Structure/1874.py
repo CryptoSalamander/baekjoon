6464edfe329696bee5b2c22d70eeee18364edf99
HYUNSOO HA <dhy03196@naver.com>
2021-11-06 01:44:31 +0900

    [ADD] 1874 with python

--- Data_Structure/1874.py
+++ Data_Structure/1874.py
@@ -4,10 +4,34 @@ def input():
     return sys.stdin.readline().rstrip()
 
 N = int(input())
-
+stack = []
+target = deque()
 answer = []
 for _ in range(N):
-    answer.append(int(input()))
+    target.append(int(input()))
+val = target.popleft()
+for i in range(1,val+1):
+    answer.append('+')
+    stack.append(i)
+answer.append('-')
+val = stack.pop()
+while target:
+    tmp = target.popleft()
 
-for i in range(1,N+1):
-    answer.append(i)
+    if val > tmp:
+        if tmp != stack[-1]:
+            answer = ["NO"]
+            break
+        else:
+            answer.append('-')
+            stack.pop()
+    else:
+        for i in range(val+1, tmp+1):
+            stack.append(i)
+            answer.append('+')
+        answer.append('-')
+        val = stack.pop()
+print("\n".join(answer))
+
+
+
