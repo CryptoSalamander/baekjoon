d268f376b955a7ba22f36c82ceee9fad9e918b87
HYUNSOO HA <dhy03196@naver.com>
2021-11-07 07:04:54 +0900

    [ADD] 11279 with python

---
+++ Data_Structure2/11279.py
@@ -0,0 +1,17 @@
+import heapq, sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    val = int(input())
+
+    if val == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (-val,val))
